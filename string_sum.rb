a82cea3a8e704f531007b76a43ac5d1d4aa9a26f
expertcoder6 <expertcoder6@gmail.com>
2025-03-28 11:54:52 +0530

    Add support for single number

--- string_sum.rb
+++ string_sum.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 class StringCalculator
   def self.addition(values)
     return 0 if values.empty?
+    values.to_i
   end
 end
 
@@ -9,6 +10,10 @@ class StringCalculatorTest < Minitest::Test
   def test_returns_zero_on_empty_string
     assert_equal 0, StringCalculator.addition("")
   end
+  def test_returns_single_number
+    assert_equal 7, StringCalculator.addition("7")
+  end
 end
 
 puts StringCalculator.addition("") # => 0
+puts StringCalculator.addition("3") # => 3
