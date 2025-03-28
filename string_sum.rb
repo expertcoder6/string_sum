806fea4649d8b20e5077df4533290cac1b9abac8
expertcoder6 <expertcoder6@gmail.com>
2025-03-28 12:10:09 +0530

    Raise exception for negative numbers

--- string_sum.rb
+++ string_sum.rb
@@ -4,6 +4,7 @@ class StringCalculator
     return 0 if values.empty?
     delimiter, values = extract_delimiter(values)
     values_array = values.split(delimiter)
+    handle_negative_value(values_array)
     values_array.map(&:to_i).sum
   end
   
@@ -14,6 +15,11 @@ class StringCalculator
     else
       [/,|\n/, values]
     end
+  end
+
+  def self.handle_negative_value(values)
+    negatives = values.select { |v| v.to_i < 0 }
+    raise "negative values not allowed: #{negatives.join(", ")}" if negatives.any?
   end
   
 end
@@ -37,7 +43,9 @@ class StringCalculatorTest < Minitest::Test
   def test_adds_with_custom_delimiter
     assert_equal 10, StringCalculator.addition("//;\n2;8")
   end
-  
+  def test_raises_on_negative_number
+    assert_raises(RuntimeError) { StringCalculator.addition("2,-3") }
+  end
 end
 
 puts StringCalculator.addition("") # => 0
@@ -46,3 +54,4 @@ puts StringCalculator.addition("7,5") # => 12
 puts StringCalculator.addition("5,6,7,8,9")# => 35
 puts StringCalculator.addition("9\n5,6") # => 20
 puts StringCalculator.addition("//;\n30;88") # => 118
+# puts StringCalculator.addition("8,-9") # unlock the line to see error
