1ddaed9292143f65ebea7b144b03fc9f87d68260
expertcoder6 <expertcoder6@gmail.com>
2025-03-28 11:56:39 +0530

    Add support for two comma-separated numbers

--- string_sum.rb
+++ string_sum.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 class StringCalculator
   def self.addition(values)
     return 0 if values.empty?
-    values.to_i
+    values.split(",").map(&:to_i).sum
   end
 end
 
@@ -13,7 +13,11 @@ class StringCalculatorTest < Minitest::Test
   def test_returns_single_number
     assert_equal 7, StringCalculator.addition("7")
   end
+  def test_adds_two_numbers
+    assert_equal 10, StringCalculator.addition("6,4")
+  end
 end
 
 puts StringCalculator.addition("") # => 0
 puts StringCalculator.addition("3") # => 3
+puts StringCalculator.addition("7,5") # => 12
