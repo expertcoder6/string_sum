fac9068273c6e5683b0d6af15f5ba0e905c3b2ac
expertcoder6 <expertcoder6@gmail.com>
2025-03-28 12:00:31 +0530

    Add support for multiple value

--- string_sum.rb
+++ string_sum.rb
@@ -16,8 +16,12 @@ class StringCalculatorTest < Minitest::Test
   def test_adds_two_numbers
     assert_equal 10, StringCalculator.addition("6,4")
   end
+  def test_addition_with_multiple_numbers
+    assert_equal 30, StringCalculator.addition("4,5,6,7,8")
+  end
 end
 
 puts StringCalculator.addition("") # => 0
 puts StringCalculator.addition("3") # => 3
 puts StringCalculator.addition("7,5") # => 12
+puts StringCalculator.addition("5,6,7,8,9")# => 35
